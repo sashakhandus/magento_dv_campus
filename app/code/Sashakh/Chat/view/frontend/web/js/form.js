28a839b1f71b62e494a64c09ee240e72a879a8e2
sashakhandus <sashakhandus@gmail.com>
2020-01-05 00:10:46 +0200

    hw-17 show messages

--- app/code/Sashakh/Chat/view/frontend/web/js/form.js
+++ app/code/Sashakh/Chat/view/frontend/web/js/form.js
@@ -43,6 +43,24 @@ define([
         },
 
         /**
+         *
+         * @param name
+         * @param message
+         */
+
+        appendMessage: function(name, message) {
+            var userName = $("<p class='user-question user-name'></p>").text(name);
+            var userMessage = $("<p class='user-question user-message'></p>").text(message);
+
+            var adminName = $("<p class='admin-question admin-name'></p>").text('admin');
+            var adminMessage = $("<p class='admin-question admin-message'></p>").text('admin message');
+
+            $('.messages-list').append(userName, userMessage, adminName, adminMessage);
+
+            $('.messages-list').show();
+        },
+
+        /**
          * Submit request via AJAX. Add form key to the post data.
          */
         ajaxSubmit: function () {
@@ -70,17 +88,14 @@ define([
                     $('body').trigger('processStop');
                     // @TODO: show new preferences
 
+                    this.appendMessage(formData.get('name'), formData.get('message'));
 
-                    $('.messages-list').append(formData.get('name'));
-                    $('.messages-list').append(formData.get('message'));
-                    $('.messages-list').show();
+                    $("#sashakh-chat-form")[0].reset();
 
                    /*alert({
                         title: $.mage.__('Success'),
                         content: $.mage.__(response.message)
                     });*/
-
-
                 },
 
                 /** @inheritdoc */
@@ -92,7 +107,8 @@ define([
                         /*eslint max-len: ["error", { "ignoreStrings": true }]*/
                         content: $.mage.__('Your preferences can\'t be saved. Please, contact us if you see this message.')
                     });
-                }
+                },
+
             });
         },
     });
