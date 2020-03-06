1f8bcdce06d01b201378e0d070660d300494e3a7
Oleksandra Khandus <oleksandrak@default-value.com>
2020-03-06 15:27:36 +0200

    hw-20 without update messages and set customer_id

--- app/code/Sashakh/Chat/Block/Messages.php
+++ app/code/Sashakh/Chat/Block/Messages.php
@@ -2,30 +2,48 @@
 
 namespace Sashakh\Chat\Block;
 
+use Magento\Framework\Event\ManagerInterface as EventManager;
 use Sashakh\Chat\Model\ResourceModel\Chat\Collection;
 
-
-class Messages  extends \Magento\Framework\View\Element\Template
-
+class Messages extends \Magento\Framework\View\Element\Template
 {
+    /**
+     * @var \Magento\Customer\Model\Session
+     */
+    private $customerSession;
+
     /**
      * @var \Sashakh\Chat\Model\ResourceModel\Chat\CollectionFactory
      */
     private $collectionFactory;
 
     /**
+     * @var EventManager
+     */
+    private $eventManager;
+
+    /**
      * Requests constructor.
      * @param \Sashakh\Chat\Model\ResourceModel\Chat\CollectionFactory $collectionFactory
      * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param \Magento\Customer\Model\Session $customerSession
      * @param array $data
      */
     public function __construct(
         \Sashakh\Chat\Model\ResourceModel\Chat\CollectionFactory $collectionFactory,
         \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Customer\Model\Session $customerSession,
+        \Magento\Framework\Event\ManagerInterface $eventManager,
         array $data = []
     ) {
         parent::__construct($context, $data);
         $this->collectionFactory = $collectionFactory;
+        $this->customerSession = $customerSession;
+    }
+
+    public function getCustomerSession()
+    {
+        return $this->customerSession;
     }
 
     /**
@@ -33,9 +51,12 @@ class Messages  extends \Magento\Framework\View\Element\Template
      */
     public function getAllMessages(): Collection
     {
+        //$this->eventManager->dispatch('customer_login', ['chat_hash' => $this->getCustomerSession()->getCustomerMessage()['chat_hash']]);
+
         /** @var Collection $collection */
         $collection = $this->collectionFactory->create();
         $collection->getSelect();
+        $collection->addFieldToFilter('chat_hash', ['eq' => $this->getCustomerSession()->getCustomerMessage()['chat_hash']]);
         $collection->setOrder('created_at', 'DESC');
 
         if ($limit = $this->getData('limit')) {
