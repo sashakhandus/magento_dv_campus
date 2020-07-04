925cb2bb945b68a807974362977a6772d4f0c217
oleksandrak@default-value <oleksandrak@default-value.com>
2020-07-04 14:03:26 +0000

    hw-22 rewrite a simple Knockout component of a form

--- app/code/Sashakh/Chat/view/frontend/requirejs-config.js
+++ app/code/Sashakh/Chat/view/frontend/requirejs-config.js
@@ -3,7 +3,6 @@ var config = {
         '*': {
             sashakh_chat_openButton: 'Sashakh_Chat/js/open-button',
             sashakh_chat_form: 'Sashakh_Chat/js/view/form',
-            sashakh_chat_messages: 'Sashakh_Chat/js/view/messages'
         }
     },
 };
--- app/code/Sashakh/Chat/view/frontend/web/js/view/form.js
+++ app/code/Sashakh/Chat/view/frontend/web/js/view/form.js
@@ -3,47 +3,76 @@ define([
     'ko',
     'uiComponent',
     'Magento_Customer/js/customer-data',
-    'Sashakh_Chat/js/action/save-messages',
+    'Magento_Ui/js/modal/alert',
     'Magento_Ui/js/modal/modal'
-], function ($, ko, Component, customerData, saveMessages) {
+], function ($, ko, Component, customerData, alert) {
     'use strict';
 
     return Component.extend({
         defaults: {
-            template: 'Sashakh_Chat/form',
-            //customerMessage: customerData.get('customer_message'),
-            action: ''
+            template: 'Sashakh_Chat/form'
         },
-       allMessages: {},
 
-        /** @inheritDoc */
-        initObzervable: function() {
-            var customerMessage = customerData.get('customer_message')();
+        inputAuthorType: ko.observable(),
+        inputAuthorName: ko.observable(),
+
+        initObservable: function () {
             this._super();
+            this.inputAuthorType.subscribe(function (typeValue) {
+                console.log(typeValue);
+            });
 
-            $(document).on(
-                $.proxy(this.openModal, this)
-            );
+            this.inputAuthorName.subscribe(function (nameValue) {
+                console.log(nameValue);
+            });
 
-           // authorType.value = ko.observable('');
-           // authorName.value = ko.observable('');
-          //  message.value = ko.observable('');
+            return this;
+        },
+
+        saveMessage: function() {
+            console.log("Save Message");
+        }
+    });
+
+    $.widget('sashakhChat.form', {
+        options: {
+            action: ''
         },
 
         /**
-         * Init modal from the component HTML
+         * @private
          */
-        initModal: function (formElement) {
-            this.modal = $(formElement).modal({
+        _create: function () {
+            this.modal = $(this.element).modal({
                 buttons: []
+            });
+
+            $(this.element).on('submit.sashakh_chat', $.proxy(this.saveMessages, this));
+
+            customerData.get('chat').subscribe(function (value) {
+                //console.log(value);
             });
         },
 
+        _destroy: function () {
+            this.modal.closeModal();
+            $(this.element).off('submit.sashakh_chat');
+            this.modal.destroy();
+        },
+
+        saveMessages: function () {
+            if (!this.validateForm()) {
+                return;
+            }
+
+            this.ajaxSubmit();
+        },
+
         /**
-         * Open modal form with chat for message
+         * Validate request form
          */
-        openModal: function () {
-            this.modal.modal('openModal');
+        validateForm: function () {
+            return $(this.element).validation().valid();
         },
 
         /**
@@ -53,164 +82,71 @@ define([
          * @param message
          */
         appendMessage: function(authorType, authorName, message) {
-            var currentDate = new Date();
-            var dateTime = currentDate.toUTCString();
-            var userDate, userName, userMessage, adminDate, adminName, adminMessage;
+            var currentDate = new Date(),
+                dateTime = currentDate.toUTCString(),
+                date, name, messageHtml, authorTypeHtml, li;
 
-            if (authorType === '1') {
-                userDate = $("<p class='user-question user-date'></p>").text(dateTime);
-                userName = $("<p class='user-question user-name'></p>").text(authorName);
-                userMessage = $("<p class='user-question user-message'></p>").text(message);
+            authorTypeHtml = authorType === '1' ? 'user' : 'admin';
+            li = $("<li class='chat-message'></li>").addClass(authorTypeHtml);
 
-                $('.messages-list').append(userDate, userName, userMessage).show();
-            } else if (authorType === '2') {
-                adminDate = $("<p class='admin-question admin-date'></p>").text(dateTime);
-                adminName = $("<p class='admin-question admin-name'></p>").text(authorName);
-                adminMessage = $("<p class='admin-question admin-message'></p>").text(message);
+            date = $("<p class='message-date'></p>").text(dateTime);
+            name = $("<p class='message-name'></p>").text(authorName);
+            messageHtml = $("<p class='message-message'></p>").text(message);
 
-                $('.messages-list').append(adminDate, adminName, adminMessage).show();
-            }
+            li.append(date, name, messageHtml);
 
+            $('.messages-list').append(li).show();
         },
 
         /**
          * Submit request via AJAX. Add form key to the post data.
          */
-        saveMessages: function () {
-            var payload = {
-                'form_key': $.mage.cookies.get('form_key'),
-                isAjax: 1
-            };
+        ajaxSubmit: function () {
+            var formData = new FormData($(this.element).get(0));
 
-            saveMessages(payload, this.action)
-                .done(function () {
-                    this.modal.modal('closeModal');
-                }.bind(this));
-        }
+            formData.append('form_key', $.mage.cookies.get('form_key'));
+            formData.append('isAjax', 1);
 
+            $.ajax({
+                url: this.options.action,
+                data: formData,
+                processData: false,
+                contentType: false,
+                type: 'post',
+                dataType: 'json',
+                context: this,
 
-        });
+                /** @inheritdoc */
+                beforeSend: function () {
+                    $('body').trigger('processStart');
+                },
+
+                /** @inheritdoc */
+                success: function (response) {
+                    $('body').trigger('processStop');
+
+                    this.appendMessage(formData.get('authorType'), formData.get('authorName'), formData.get('message'));
+
+                    $("#sashakh-chat-form").get(0).reset();
+
+                    alert({
+                        title: $.mage.__('Success'),
+                        content: response.message
+                    });
+                },
+
+                /** @inheritdoc */
+                error: function () {
+                    $('body').trigger('processStop');
+                    alert({
+                        title: $.mage.__('Error'),
+                        content: $.mage.__('Your preferences can\'t be saved. Please, contact us if you see this message.')
+                    });
+                },
+
+            });
+        },
     });
 
-   // $.widget('sashakhChat.form', {
-//    options: {
-   //        action: ''
-   //     },
-
-        /**
-         * @private
-         */
-    //    _create: function () {
-    //        this.modal = $(this.element).modal({
-    //            buttons: []
-    //        });
-
-     //       $(this.element).on('submit.sashakh_chat', $.proxy(this.saveMessages, this));
-
-     //       console.log(customerData.get('chat')());
-     //       customerData.get('chat').subscribe(function (value) {
-     //           console.log(value);
-    //        });
-    //    },
-
-     //   _destroy: function () {
-     //       this.modal.closeModal();
-    //        $(this.element).off('submit.sashakh_chat');
-    //        this.modal.destroy();
-    //    },
-
-     //   saveMessages: function () {
-     //       if (!this.validateForm()) {
-    //            return;
-    //        }
-
-    //       this.ajaxSubmit();
-    //    },
-
-        /**
-         * Validate request form
-         */
-     //   validateForm: function () {
-     //       return $(this.element).validation().valid();
-     //   },
-
-        /**
-         *
-         * @param authorType
-         * @param authorName
-         * @param message
-         */
-     //   appendMessage: function(authorType, authorName, message) {
-     //       var currentDate = new Date();
-     //       var dateTime = currentDate.toUTCString();
-      //      var userDate, userName, userMessage, adminDate, adminName, adminMessage;
-
-     //       if (authorType === '1') {
-     //           userDate = $("<p class='user-question user-date'></p>").text(dateTime);
-     //           userName = $("<p class='user-question user-name'></p>").text(authorName);
-     //           userMessage = $("<p class='user-question user-message'></p>").text(message);
-
-     //           $('.messages-list').append(userDate, userName, userMessage).show();
-     //       } else if (authorType === '2') {
-     //           adminDate = $("<p class='admin-question admin-date'></p>").text(dateTime);
-     //           adminName = $("<p class='admin-question admin-name'></p>").text(authorName);
-     //           adminMessage = $("<p class='admin-question admin-message'></p>").text(message);
-
-    //            $('.messages-list').append(adminDate, adminName, adminMessage).show();
-     //       }
-
-     //   },
-
-        /**
-         * Submit request via AJAX. Add form key to the post data.
-         */
-    //    ajaxSubmit: function () {
-     //       var formData = new FormData($(this.element).get(0));
-
-        //      formData.append('form_key', $.mage.cookies.get('form_key'));
-     //       formData.append('isAjax', 1);
-
-     //       $.ajax({
-     //           url: this.options.action,
-     //           data: formData,
-     //           processData: false,
-     //           contentType: false,
-     //           type: 'post',
-      //          dataType: 'json',
-      //          context: this,
-
-                /** @inheritdoc */
-     //           beforeSend: function () {
-    //                $('body').trigger('processStart');
-     //           },
-
-                /** @inheritdoc */
-     //           success: function (response) {
-     //               $('body').trigger('processStop');
-
-      //              this.appendMessage(formData.get('authorType'), formData.get('authorName'), formData.get('message'));
-
-     //               $("#sashakh-chat-form")[0].reset();
-
-      //             alert({
-     //                   title: $.mage.__('Success'),
-     //                   content: response.message
-     //               });
-     //           },
-
-                /** @inheritdoc */
-     //           error: function () {
-     //               $('body').trigger('processStop');
-      //              alert({
-     //                   title: $.mage.__('Error'),
-                        /*eslint max-len: ["error", { "ignoreStrings": true }]*/
-     //                   content: $.mage.__('Your preferences can\'t be saved. Please, contact us if you see this message.')
-     //               });
-     //           },
-
-     //       });
-     //   },
-    //});
-
-    //return $.sashakhChat.form;
-//});
+    return $.sashakhChat.form;
+});
